baa34e77d23ab0de32179257520cd15ef39e7f80
josealonzo2004 <josealonzohp@gmail.com>
2024-05-18 22:23:21 -0500

    normal

--- tasks/form.py
+++ tasks/form.py
@@ -4,10 +4,9 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'important', 'cuerpo']
+        fields = ['title', 'description', 'important']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Write a title'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write a description'}),
-            'cuerpo': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write a body'}),
             'important': forms.CheckboxInput(attrs={'class': 'form-check-imput m-auto'})
         }
