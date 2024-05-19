9e0624e3b712b16e5b17adc2b5e297a69be582ee
josealonzo2004 <josealonzohp@gmail.com>
2024-05-18 21:32:18 -0500

    ya estaria

--- tasks/models.py
+++ tasks/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    cuerpo = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
     datecomplete = models.DateTimeField(null=True, blank=True)
     important = models.BooleanField(default=False)
