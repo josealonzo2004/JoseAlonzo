b738928a9479e45bf35c2ed6761131f502ad7802
josealonzo2004 <josealonzohp@gmail.com>
2024-05-18 21:20:29 -0500

    modelo1

--- tasks/form.py
+++ tasks/form.py
@@ -4,7 +4,7 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'important']
+        fields = ['title', 'description', 'important', 'cuerpo']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Write a title'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write a description'}),
